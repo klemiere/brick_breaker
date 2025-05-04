e18d833db1f91eda42ef9579e91e63c97b01caef
klemiere <lemierekevin1@gmail.com>
2025-05-04 18:24:12 +0200

    add collision checks with the screen border

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ import java.awt.*;
 
 public class Player {
     int positionX, positionY, width, height;
+    final float OFFSET = (float)width / 2;
 
     Player(int positionX, int positionY, int width, int height){
         this.positionX = positionX;
@@ -15,6 +16,17 @@ public class Player {
         g.drawRect(positionX, positionY, width, height);
     }
 
+    void checkCollisions() {
+        // Check for screen borders
+        // Left and right borders
+        if (positionX <= 0) {
+            positionX = (int) OFFSET;
+        } else if (positionX + width >= GamePanel.SCREEN_WIDTH) {
+            positionX = GamePanel.SCREEN_WIDTH - width - 1; //magic number to keep the player rectangle right border on screen
+
+        }
+    }
+
     void move(char direction){
         switch(direction){
             case 'L':
@@ -26,5 +38,6 @@ public class Player {
             case 'N':
                 break;
         }
+        checkCollisions();
     }
 }
