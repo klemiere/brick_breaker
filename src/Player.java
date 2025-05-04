ff8f2d5b7a1e0b201d8b0c179e244b0c499e3b06
klemiere <lemierekevin1@gmail.com>
2025-05-04 16:25:26 +0200

    Add constructor and draw and move methods

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
 public class Player {
+    int positionX, positionY, width, height;
+
+    Player(int positionX, int positionY, int width, int height){
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+    }
+
+    void drawPlayer(Graphics g){
+        g.setColor(Color.WHITE);
+        g.drawRect(positionX, positionY, width, height);
+    }
+
+    void move(char direction){
+        switch(direction){
+            case 'L':
+                positionX -= 5;
+                break;
+            case 'R':
+                positionX += 5;
+                break;
+            case 'N':
+                break;
+        }
+    }
 }
